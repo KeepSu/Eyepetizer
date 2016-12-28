f5451ad11f30491f6a19fa17d30050663c21a0f2
inSkyFly <799139668@qq.com>
2016-12-28 19:16:20 +0800

    1:关注Fragment添加了一些控件以及布局文件修改 2:添加了关注页面多布局中的一种布局页面 3:shape画了一个关注的圆形按钮

--- app/src/main/java/com/tzs/eyepetizer/fragment/FollowFragment.java
+++ app/src/main/java/com/tzs/eyepetizer/fragment/FollowFragment.java
@@ -3,28 +3,41 @@ package com.tzs.eyepetizer.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.tzs.eyepetizer.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 关注页面
  */
 public class FollowFragment extends Fragment {
-
-
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.tv_author)
+    TextView tv_author;
+    @BindView(R.id.iv_search)
+    ImageView iv_search;
     public FollowFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_follow, container, false);
+        View view = inflater.inflate(R.layout.fragment_follow, container, false);
+        //解析注解
+        ButterKnife.bind(this,view);
+
+        return view;
     }
+
 
 }
