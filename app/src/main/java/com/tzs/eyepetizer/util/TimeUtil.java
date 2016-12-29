dec09ac454e98438246addcf7e2a11aee3dc6335
lin0000 <1184617653@qq.com>
2016-12-29 20:01:30 +0800

    时间工具类

---
+++ app/src/main/java/com/tzs/eyepetizer/util/TimeUtil.java
@@ -0,0 +1,27 @@
+package com.tzs.eyepetizer.util;
+
+/**
+ * Created by h on 2016/12/29.
+ */
+
+public class TimeUtil {
+
+    /**
+     * 把int类型的事件转换成01'20"的格式
+     *
+     * @param time
+     * @return
+     */
+    public static String getDurnig(int time) {
+        String during = "";
+        if (time / 60 < 10) {
+            during = "0";
+        }
+        during = during + time / 60 + "'";
+        if (time % 60 < 10) {
+            during = during + "0";
+        }
+        during = during + time % 60 + "\"";
+        return during;
+    }
+}
