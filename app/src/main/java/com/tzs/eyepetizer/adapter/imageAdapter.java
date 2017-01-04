a0862ea315589cc1c715474387c0ed72253c3589
TZS <keepsu@outlook.com>
2017-01-04 12:30:14 +0800

    新增viewpager的图片适配器

---
+++ app/src/main/java/com/tzs/eyepetizer/adapter/imageAdapter.java
@@ -0,0 +1,51 @@
+package com.tzs.eyepetizer.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.tzs.eyepetizer.util.ImageUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by TZS on 2017/1/3.
+ */
+
+public class imageAdapter extends PagerAdapter {
+    private List<String> data = new ArrayList();
+    private Context mContext;
+
+    public imageAdapter(List data, Context context) {
+        this.data = data;
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+//        return data.size();
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView img = new ImageView(mContext);
+        img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        ImageUtil.setImage(mContext, data.get(position % data.size()), img);
+        container.addView(img);
+        return img;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ImageView) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
